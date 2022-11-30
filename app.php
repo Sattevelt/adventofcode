69f546de7d7a6682a62aa80345026f54a96732ef
Steven Attevelt <steven.attevelt@sawiday.com>
2022-11-30 14:00:43 +0100

    trigger check

--- app.php
+++ app.php
@@ -7,4 +7,4 @@ Config::set('basePath', __DIR__);
 // Set the session id if you want to automatically download puzzle inputs for assignments.
 // Can easily be taken from your browser cookies when logged in to adventofcode.com
 Config::set('aocSessionId', '');
-App::run($argv);
+App::run($_SERVER['argv']);
